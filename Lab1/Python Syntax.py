78f9819177c92929fd8a675b59070a6ba39c6d4f
keletos <156783563+keletos@users.noreply.github.com>
2024-01-24 23:32:30 +0600

    Add files via upload

---
+++ Lab1/Python Syntax.py
@@ -0,0 +1,14 @@
+#if
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+#Variables in Python:
+x = 5
+y = "Hello, World!"
+
+#Comments in Python:
+#This is a comment.
+print("Hello, World!")
+
